780c8221cb8dc604d88e34230e5d43d946ed1be6
tafflan <3219599757@qq.com>
2023-06-09 06:09:58 +0000

    Mount the finetuning functional code dir to docker

--- docker_run/fastchat_docker.sh
+++ docker_run/fastchat_docker.sh
@@ -2,6 +2,7 @@ docker run -dt --name fastchat --restart=always --gpus all --network=host \
 -v <Your code directory>:/code \
 -v <Your model and dataset directory>:/hf \
 -v <Your output directory>:/output \
+-v ./finetune/fastchat:/code/easy_llm_finetuner \
 -w /code \
 tafflan/llm_dockers:fastchat-0.0.2 \
 /bin/bash
--- docker_run/qlora_docker.sh
+++ docker_run/qlora_docker.sh
@@ -2,6 +2,7 @@ docker run -dt --name qlora --restart=always --gpus all --network=host \
 -v <Your code directory>:/code \
 -v <Your model and dataset directory>:/hf \
 -v <Your output directory>:/output \
+-v Easy-LLM-Finetuner/finetune/qlora:/code/easy_llm_finetuner \
 -w /code \
 tafflan/llm_dockers:qlora-0.0.1 \
 /bin/bash
